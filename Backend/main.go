b46975001a4e687033522f444b03db2b5026b8ad
mdhsaikats <saikatsiker2911@gmail.com>
2026-03-07 22:06:59 +0600

    implement the chi router

---
+++ Backend/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func main() {
+    r := chi.NewRouter()
+    r.Use(middleware.Logger)
+    r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+        w.Write([]byte("Hello World!"))
+    })
+    err := http.ListenAndServe(":3000", r)
+	if err != nil {
+		fmt.Println("Server crushed")
+	}
+}
