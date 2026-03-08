1e2e74e7803fec1d412bc494ecb1323b1448c86e
mdhsaikats <saikatsiker2911@gmail.com>
2026-03-08 22:44:38 +0600

    implemented the js code for the login form

---
+++ Frontend/js/registration.js
@@ -0,0 +1,26 @@
+import { BASE_URL } from "./config.js";
+
+document.getElementById("loginForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  fetch(`${BASE_URL}\login`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      email: email,
+      password: password,
+    }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("Success:", data);
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+});
